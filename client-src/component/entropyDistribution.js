011e892e109b489fbb41c14c8f77c1bea8b8e8ee
WuYifanX <510182912@qq.com>
2018-02-27 13:40:38 +0800

    highlight function finished
    highlight function finished

--- client-src/component/entropyDistribution.js
+++ client-src/component/entropyDistribution.js
@@ -7,8 +7,12 @@ import progressToggle from './progressHandler';
 const entropyDistributionInstance = echarts.init(document.getElementById('entropy-distribution'));
 let option;
 const loadingOption = config.loading;
+const FIXEDNUMBER = 2;
 
 import entropyKDE from './../mockdata/entropyKDE';
+
+let xAxisForMeal, yAxisForMeal, xAxisForShower, yAxisForShower;
+
 
 /**
  * 模块初始化，显示 loading 效果，等待数据reload;
@@ -25,36 +29,35 @@ export function init() {
  * @returns {{xAxisForMeal: Array, yAxisForMeal: Array, xAxisForShower: Array, yAxisForShower: Array}}
  */
 const transformDataForEcharts = function (data) {
-    debugger;
 
-    const transformData = function(data){
+    const transformData = function (data) {
         const transformDataObject = {};
         let sortedKeys = Object.keys(data).sort(function (a, b) {
             return parseFloat(a) - parseFloat(b);
         });
 
         sortedKeys.forEach(function (key) {
-            let fixedKey = parseFloat(key).toFixed(2);
+            let fixedKey = parseFloat(key).toFixed(FIXEDNUMBER);
             let fixedValue = parseFloat(data[key]);
-            if(transformDataObject[fixedKey] = null){
+            if (transformDataObject[fixedKey] = null) {
                 transformDataObject[fixedKey] = fixedValue;
-            }else{
+            } else {
                 transformDataObject[fixedKey] += fixedValue;
             }
         });
 
-        const xAxis = Object.keys(transformDataObject).sort(function(a,b){
+        const xAxis = Object.keys(transformDataObject).sort(function (a, b) {
             return parseFloat(a) - parseFloat(b);
         });
-        const yAxis = xAxis.map(function(key){
+        const yAxis = xAxis.map(function (key) {
             return transformDataObject[key]
         });
-        return [xAxis,yAxis];
+        return [xAxis, yAxis];
     };
-    const [xAxisForMeal,yAxisForMeal] = transformData(data['meal']);
-    const [xAxisForShower,yAxisForShower] = transformData(data['shower']);
+    const [xAxisForMeal, yAxisForMeal] = transformData(data['meal']);
+    const [xAxisForShower, yAxisForShower] = transformData(data['shower']);
 
-    return {xAxisForMeal, yAxisForMeal, xAxisForShower, yAxisForShower}
+    return [xAxisForMeal, yAxisForMeal, xAxisForShower, yAxisForShower];
 };
 
 
@@ -63,9 +66,8 @@ const transformDataForEcharts = function (data) {
  */
 export function reloadData() {
 
-    const {xAxisForMeal, yAxisForMeal, xAxisForShower, yAxisForShower} = transformDataForEcharts(entropyKDE);
+    [xAxisForMeal, yAxisForMeal, xAxisForShower, yAxisForShower] = transformDataForEcharts(entropyKDE);
 
-    debugger;
     option = {
         backgroundColor: config.defaultColor.cardColor,
         xAxis: {
@@ -79,15 +81,15 @@ export function reloadData() {
             }
         },
         legend: {
-            show:false,
+            show: false,
             data: ['Meal', 'Shower'],
-            right:10,
-            top:40,
-            orient:'vertical',
-            textStyle:{
-                color:config.defaultColor.textColor
+            right: 10,
+            top: 40,
+            orient: 'vertical',
+            textStyle: {
+                color: config.defaultColor.textColor
             },
-            align:'right',
+            align: 'right',
 
         },
         yAxis: {
@@ -111,10 +113,9 @@ export function reloadData() {
         },
         brush: {
             xAxisIndex: 'all',
-            inBrush:
-                {
-                    color: '#222'
-                },
+            inBrush: {
+                color: '#222'
+            },
             outOfBrush: {
                 colorAlpha: 0
             }
@@ -138,9 +139,11 @@ export function reloadData() {
                 data: yAxisForMeal,
                 itemStyle: {
                     normal: {
-                        color: config.defaultColor.cardColor,
+                        color: config.defaultColor.cardColor
+                    },
+                    emphasis: {
+                        color: config.defaultColor.highlightColor,
                         barBorderRadius: 10
-
                     }
                 },
                 barWidth: '100%',
@@ -163,7 +166,10 @@ export function reloadData() {
                 data: yAxisForShower,
                 itemStyle: {
                     normal: {
-                        color: config.defaultColor.cardColor,
+                        color: config.defaultColor.cardColor
+                    },
+                    emphasis: {
+                        color: config.defaultColor.highlightSecondColor,
                         barBorderRadius: 10
                     }
                 },
@@ -194,13 +200,38 @@ export function updateData(data) {
     entropyDistributionInstance.showLoading(loadingOption);
 }
 
+/**
+ * 传入一个order的Array然后来高亮
+ * @param orderArray
+ */
+export function highlightData(orderArray) {
+    let mealHighlightIndex = [], showerHighlightIndex = [];
+
+    if (orderArray['meal'] != null) {
+        orderArray['meal']['highlight'].forEach(function (order) {
+
+            order = parseFloat(order).toFixed(FIXEDNUMBER);
+            mealHighlightIndex.push(xAxisForMeal.indexOf(order));
+        });
+
+        entropyDistributionInstance.dispatchAction({
+            type: 'highlight',
+            // 可选，系列 index，可以是一个数组指定多个系列
+            seriesIndex: 1,
+            // 可选，系列名称，可以是一个数组指定多个系列
+            seriesName: 'Meal Bar',
+            // 可选，数据的 index
+            dataIndex: mealHighlightIndex,
+        })
+    }
+}
 
 
 /**
  * 绑定brush events
  */
-const bindInstanceWithBrush = function(){
-    entropyDistributionInstance.on('brushSelected',function(e){
+const bindInstanceWithBrush = function () {
+    entropyDistributionInstance.on('brushSelected', function (e) {
         console.log(e);
     })
 };
