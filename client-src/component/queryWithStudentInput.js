8b2494e1390a49c4ad52877b2fdb7667e773d0b1
octopustail <494479914@qq.com>
2018-03-22 15:55:51 +0800

    可以从搜索栏中输入学号以及在checkbox中点选联合搜索。

--- client-src/component/queryWithStudentInput.js
+++ client-src/component/queryWithStudentInput.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import {queryServerWithTerm} from './calendarView';
+
 const init = function(){
     bindClickBtn();
 };
@@ -9,10 +10,15 @@ const bindClickBtn = function(){
     const $searchInput = $('#inputSmall');
     $searchBtn.click(function(){
         const value = $searchInput[0].getAttribute('studentid');
-        if(!value){
+        const inputvalue = $searchInput[0].value;
+       //直接将从input中输入获得的值和从checkbox中点选得到的值连接成一个字符串就可以
+        let searchvalue = value + ','+inputvalue;
+
+
+        if(!searchvalue){
             return null
         }
-        queryServerWithTerm(value);
+        queryServerWithTerm(searchvalue);
     })
 
 };
